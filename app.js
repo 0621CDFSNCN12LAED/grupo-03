b67d824730f19232905ea41590916c306d807030
sebasla76 <sebasla76@hotmail.com>
2021-08-17 19:52:50 -0300

    app.js con links

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ app.listen(3050, () => {
     console.log('Servidor corriendo en el puerto 3050');
 });
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/indexSA.html'));
+});
+app.get('/views/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+app.get('/views/productCart.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productCart.html'));
 });
