5e7b7cbd9ec971b040ae2f2453c6a731cc12c659
sebasla76 <sebasla76@hotmail.com>
2021-08-21 19:28:50 -0300

    entrega segundo sprint proyecto integrador grupo3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(3050, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/indexSA.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
